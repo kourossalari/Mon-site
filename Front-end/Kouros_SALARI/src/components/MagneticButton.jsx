faf6d409dc5c65deecd7238fe106b77ecdcf2a4e
saalarikouros@gmail.com <saalarikouros@gmail.com>
2026-07-23 14:22:20 +0200

    feat: add magneticButton & particle components

---
+++ Front-end/Kouros_SALARI/src/components/MagneticButton.jsx
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+
+function MagneticButton({ children, className, ...props }) {
+
+    const btnRef = useRef(null)
+
+    function handleMouseMove (e) {
+        const r = btnRef.current.getBoundingClientRect()
+        const x = e.clientX - r.left - r.width / 2
+        const y = e.clientY - r.top - r.height / 2
+        btnRef.current.style.transform = `translate(${x * 0.25}px, ${y * 0.4}px)`
+    }
+
+    function handleMouseLeave() {
+    btnRef.current.style.transform = "translate(0,0)";
+    }
+
+    return(
+        <span 
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        >
+            <button ref={btnRef} className={className} {...props}>
+                {children}
+            </button>
+        </span>
+    )
+}
+
+export default MagneticButton
