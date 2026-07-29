edd1073b7b861dc3540cdb38680a65734a4f2852
saalarikouros@gmail.com <saalarikouros@gmail.com>
2026-07-29 17:58:07 +0200

    clean page

--- Back-end/config/mailer.js
+++ Back-end/config/mailer.js
@@ -11,8 +11,6 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASS,
   },
 });
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 
 export default transporter;
--- Back-end/server.js
+++ Back-end/server.js
@@ -16,8 +16,6 @@ app.use(cors({
   credentials: true,
 }));
 
-app.options("/*", cors());
-
 app.use(express.json());
 
 app.use("/api/contact", contactRouter);
