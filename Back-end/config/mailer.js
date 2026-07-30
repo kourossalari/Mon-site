e041ef0b39d24637551fd5a7b97c24cc608a8a68
saalarikouros@gmail.com <saalarikouros@gmail.com>
2026-07-30 12:54:35 +0200

    feat: try new host

--- Back-end/config/mailer.js
+++ Back-end/config/mailer.js
@@ -1,16 +1,13 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-
 
 export default transporter;
