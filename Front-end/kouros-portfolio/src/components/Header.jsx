14ceb4ad9a8a553485170526761f08a7f72c627c
saalarikouros@gmail.com <saalarikouros@gmail.com>
2026-07-30 14:51:42 +0200

    feat: managemnt media

--- Front-end/kouros-portfolio/src/components/Header.jsx
+++ Front-end/kouros-portfolio/src/components/Header.jsx
@@ -9,9 +9,9 @@ export default function Header() {
       </div>
       <nav>
         <Link href="/">Accueil</Link>
-        <Link href="/#projets">Projets</Link>
-        <Link href="/#competences">Compétences</Link>
-        <Link href="/#parcours">Parcours</Link>
+        <Link href="/#projets" className="linkMedia">Projets</Link>
+        <Link href="/#competences" className="linkMedia">Compétences</Link>
+        <Link href="/#parcours" className="linkMedia">Parcours</Link>
       </nav>
       <Link href="/contact">
         <MagneticButton className="btn btn-primary">Me contacter</MagneticButton>
