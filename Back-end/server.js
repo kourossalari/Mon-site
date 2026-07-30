aa8d5d52344497bc27322d072c7400697e32bf70
saalarikouros@gmail.com <saalarikouros@gmail.com>
2026-07-30 17:59:03 +0200

    feat: server.js update

--- Back-end/server.js
+++ Back-end/server.js
@@ -8,13 +8,27 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [
-    "https://mon-site-17aw.vercel.app",
-    "http://localhost:3000"
-  ],
-  methods: ["GET", "POST", "OPTIONS"],
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "https://mon-site-17aw.vercel.app",
+      "http://localhost:3000"
+    ];
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST"],
   credentials: true,
 }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(express.json());
 
